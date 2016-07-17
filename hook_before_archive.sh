c10cd64028887a2adaeed43ec0bc058f1612b19c
xcorex <xcorex@MANTICORE.local>
2016-07-17 21:13:24 +0700

    add hook to rebranding Teeny libraries

---
+++ hook_before_archive.sh
@@ -0,0 +1,13 @@
+echo "-- rebranding libraries --"
+export LC_CTYPE=C LANG=C
+
+curr_dir=$(pwd)
+
+cd "avr/libraries/"
+
+#rename variable inside library
+find ./Teeny*/ -type f -name '*.*' -exec sed -i '' s/Teeny/Coco/ {} +
+#rename folder name
+find ./Teeny* -depth -name "*Teeny*" -execdir sh -c 'mv {} $(echo {} | sed "s/Teeny/Coco/")' \;
+
+cd ${curr_dir}
