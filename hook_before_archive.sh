5b5c5c365a9a9ae9883be632679f3142503e88a1
xcorex <xcorex@MANTICORE.local>
2016-07-17 22:56:49 +0700

    fix mmv rec

--- hook_before_archive.sh
+++ hook_before_archive.sh
@@ -33,7 +33,7 @@ find . -name '*Teeny*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/Teeny/Coco}"
 
 ls -al ./CocoKeyboard/examples/CocoKeyboard/
 
-mmv ";Teeny*" "Coco#1"
+mmv ";Teeny*" "#1Coco#2"
 
 
 if check_file_exists "./CocoKeyboard/examples/CocoKeyboard/TeenyKeyboard.ino"; then
