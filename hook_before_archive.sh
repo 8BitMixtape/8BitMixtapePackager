ea55e9a84c6919812912c346f28d2033fbae27fc
xcorex <xcorex@MANTICORE.local>
2016-07-17 22:19:53 +0700

    test

--- hook_before_archive.sh
+++ hook_before_archive.sh
@@ -13,13 +13,21 @@ find ./Teeny*/ -type f -exec sed -i 's/TEENY/COCO/g' '{}' \;
 #rename folder name
 # find ./Teeny* -depth -name "*Teeny*" -execdir sh -c 'mv {} $(echo {} | sed "s/Teeny/Coco/")' \;
 # find . -name "*Teeny*" -exec rename 's/Teeny$/Coco/' '{}' \;
-find . -iname "*Teeny*" -exec rename Teeny Coco '{}' \;
-find . -iname "*Teeny*" -exec rename _dbg.txt .txt '{}' \;
-find . -name Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
-find . -name *Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
+# find . -iname "*Teeny*" -exec rename Teeny Coco '{}' \;
+# find . -iname "*Teeny*" -exec rename _dbg.txt .txt '{}' \;
+# find . -name Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
+# find . -name *Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
 
 shopt -s globstar
 rename 's/Teeny/Coco/' **
-rename Teeny Coco **/*dbg*
+# rename Teeny Coco **/*dbg*
+
+for file in $(find . -name "*Teeny*")
+do 
+  echo mv $file `echo $file | sed s/Teeny$/Coco/`
+done
+
+find . -name "*Teeny*" -exec bash -c 'mv "$1" "$(sed "s/\Teeny$/Coco/" <<< "$1")"' - '{}' \;
+
 
 cd ${curr_dir}
