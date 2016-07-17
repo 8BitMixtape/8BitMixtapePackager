290aad6948f80bd4ec6ea4d3f8fb46e42dc20330
xcorex <xcorex@MANTICORE.local>
2016-07-17 21:28:44 +0700

    change rename method

--- hook_before_archive.sh
+++ hook_before_archive.sh
@@ -6,7 +6,9 @@ curr_dir=$(pwd)
 cd "avr/libraries/"
 
 #rename variable inside library
-find ./Teeny*/ -type f -name '*.*' -exec sed -i '' s/Teeny/Coco/ {} +
+# find ./Teeny*/ -type f -name '*.*' -exec sed -i '' s/Teeny/Coco/ {} +
+find ./Teeny*/ -type f -exec sed -i 's/Teeny/Coco/g' '{}' \;
+
 #rename folder name
 find ./Teeny* -depth -name "*Teeny*" -execdir sh -c 'mv {} $(echo {} | sed "s/Teeny/Coco/")' \;
 
