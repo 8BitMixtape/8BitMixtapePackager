5430ea773fb5f223b1200abbeed9510eb8a384ac
xcorex <xcorex@MANTICORE.local>
2016-07-17 21:35:32 +0700

    change rename folder method

--- hook_before_archive.sh
+++ hook_before_archive.sh
@@ -8,8 +8,11 @@ cd "avr/libraries/"
 #rename variable inside library
 # find ./Teeny*/ -type f -name '*.*' -exec sed -i '' s/Teeny/Coco/ {} +
 find ./Teeny*/ -type f -exec sed -i 's/Teeny/Coco/g' '{}' \;
+find ./Teeny*/ -type f -exec sed -i 's/TEENY/COCO/g' '{}' \;
 
 #rename folder name
-find ./Teeny* -depth -name "*Teeny*" -execdir sh -c 'mv {} $(echo {} | sed "s/Teeny/Coco/")' \;
+# find ./Teeny* -depth -name "*Teeny*" -execdir sh -c 'mv {} $(echo {} | sed "s/Teeny/Coco/")' \;
+shopt -s globstar
+rename -n 's/special/regular/' **
 
 cd ${curr_dir}
