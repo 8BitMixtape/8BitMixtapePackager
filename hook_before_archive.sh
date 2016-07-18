ebbeabd0eef9f603fbb7992a4922059363f8c0a0
xcorex <xcorex@MANTICORE.local>
2016-07-18 10:17:30 +0700

    remove auto rebranding

--- hook_before_archive.sh
+++ hook_before_archive.sh
@@ -1,46 +1,46 @@
-echo "-- rebranding libraries --"
-export LC_CTYPE=C LANG=C
+# echo "-- rebranding libraries --"
+# export LC_CTYPE=C LANG=C
 
-curr_dir=$(pwd)
+# curr_dir=$(pwd)
 
-cd "avr/libraries/"
+# cd "avr/libraries/"
 
-#rename variable inside library
-# find ./Teeny*/ -type f -name '*.*' -exec sed -i '' s/Teeny/Coco/ {} +
-find ./Teeny*/ -type f -exec sed -i 's/Teeny/Coco/g' '{}' \;
-find ./Teeny*/ -type f -exec sed -i 's/TEENY/COCO/g' '{}' \;
+# #rename variable inside library
+# # find ./Teeny*/ -type f -name '*.*' -exec sed -i '' s/Teeny/Coco/ {} +
+# find ./Teeny*/ -type f -exec sed -i 's/Teeny/Coco/g' '{}' \;
+# find ./Teeny*/ -type f -exec sed -i 's/TEENY/COCO/g' '{}' \;
 
-#rename folder name
-# find ./Teeny* -depth -name "*Teeny*" -execdir sh -c 'mv {} $(echo {} | sed "s/Teeny/Coco/")' \;
-# find . -name "*Teeny*" -exec rename 's/Teeny$/Coco/' '{}' \;
-# find . -iname "*Teeny*" -exec rename Teeny Coco '{}' \;
-# find . -iname "*Teeny*" -exec rename _dbg.txt .txt '{}' \;
-# find . -name Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
-# find . -name *Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
+# #rename folder name
+# # find ./Teeny* -depth -name "*Teeny*" -execdir sh -c 'mv {} $(echo {} | sed "s/Teeny/Coco/")' \;
+# # find . -name "*Teeny*" -exec rename 's/Teeny$/Coco/' '{}' \;
+# # find . -iname "*Teeny*" -exec rename Teeny Coco '{}' \;
+# # find . -iname "*Teeny*" -exec rename _dbg.txt .txt '{}' \;
+# # find . -name Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
+# # find . -name *Teeny* | sed -e "p;s/Teeny/Coco/" | xargs -n2 mv
 
-shopt -s globstar
-rename 's/Teeny/Coco/' **
-# rename Teeny Coco **/*dbg*
+# shopt -s globstar
+# rename 's/Teeny/Coco/' **
+# # rename Teeny Coco **/*dbg*
 
-# for file in $(find ./** -name "*Teeny*")
-# do 
-#   echo mv $file `echo $file | sed s/Teeny$/Coco/`
-# done
+# # for file in $(find ./** -name "*Teeny*")
+# # do 
+# #   echo mv $file `echo $file | sed s/Teeny$/Coco/`
+# # done
 
-# find ./** -name "*Teeny*" -exec bash -c 'mv "$1" "$(sed "s/\Teeny$/Coco/" <<< "$1")"' - '{}' \;
+# # find ./** -name "*Teeny*" -exec bash -c 'mv "$1" "$(sed "s/\Teeny$/Coco/" <<< "$1")"' - '{}' \;
 
-find . -name '*Teeny*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/Teeny/Coco}"'
+# find . -name '*Teeny*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/Teeny/Coco}"'
 
-ls -al ./CocoKeyboard/examples/CocoKeyboard/
+# ls -al ./CocoKeyboard/examples/CocoKeyboard/
 
-mmv ";Teeny*" "#1Coco#2"
+# mmv ";Teeny*" "#1Coco#2"
 
-ls -al ./CocoKeyboard/examples/CocoKeyboard/
-
-
-# if check_file_exists "./CocoKeyboard/examples/CocoKeyboard/TeenyKeyboard.ino"; then
-# exit 1
-# fi
+# ls -al ./CocoKeyboard/examples/CocoKeyboard/
 
 
-cd ${curr_dir}
+# # if check_file_exists "./CocoKeyboard/examples/CocoKeyboard/TeenyKeyboard.ino"; then
+# # exit 1
+# # fi
+
+
+# cd ${curr_dir}
